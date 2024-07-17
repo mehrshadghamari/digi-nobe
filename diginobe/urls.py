aa3b804da56a7b70c2001b9f977a768024b1511e
ghamarimehrshad1 <mehrshad2014ghamari@gmail.com>
2024-07-18 02:30:27 +0330

    delete media root from url for debug mode

--- diginobe/urls.py
+++ diginobe/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path("", include("apps.patient.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
